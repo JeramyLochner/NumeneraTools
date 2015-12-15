03f574befba414c0549cc8eff28330bc1dc0c834
Jeramy Lochner <jeramy.lochner@dynetics.com>
2015-12-15 16:58:56 -0600

    Test.py | Made more things not hard coded
    Test.py is the start of a GUI for creation in Tkinter. Moved
    moves_allowed to a config file.

--- character_generator/test.py
+++ character_generator/test.py
@@ -5,6 +5,7 @@
 
 import configparser
 import random
+from tkinter import *
 
 """ Used to get user input on console
 Params:
@@ -59,6 +60,7 @@ class Character:
     # Moveset
     skills = []
     moves = []
+    move_allowed = []
     possible = []
 
     # Recommendations
@@ -82,19 +84,20 @@ class Character:
     def __str__(self):
         s = ""
         s += ('You are %s, the %s %s who %s\n' % (self.name, self.descriptor, self.clas.title(), self.focus))
+        s += ('You are Tier %d\n' % (self.tier))
         s += ('Might: %d | %d\n' % (self.pools['might'], self.edge['might']))
         s += ('Speed: %d | %d\n' % (self.pools['speed'], self.edge['speed']))
         s += ('Intellect: %d | %d\n' % (self.pools['intellect'], self.edge['intellect']))
         s += ('Skills:\n')
         for skill in self.skills:
             s += ('\t%s\n' % skill)
-        s += ('Moves:\n')
+        s += ('\nMoves:\n')
         for move in self.moves:
             s += ('\t%s\n' % move)
-        s += ('Equipment:\n')
+        s += ('\nEquipment:\n')
         for equip in self.equipment:
             s += ('\t%s\n' % equip)
-        s += ('You probably look like:\n')
+        s += ('\nYou probably look like:\n')
         for appear in self.appearance:
             s += ('\t%s\n' % appear)
         return s
@@ -118,8 +121,10 @@ class Character:
         def_move = dict(class_conf['default_moves'])
         for key,move in def_move.items():
             self.moves.append(str(key).title() + ' - ' +str(move))
-        if 'default_skills' in class_conf.sections():
-            self.skills.append(get_input("Pick a skill. (If no options, type anything)", class_conf['default_skills']['Skills'].split(',')))
+            
+        default_conf = configparser.ConfigParser()
+        default_conf.read('default.ini')
+        move_allowed = [int(default_conf['New_moves']['tier'+str(x)]) for x in range(1, 7)]
         self.get_possible_moves(self.clas, self.tier)
 
     """ Change might, intellect, or speed by a certain value (usually 1) """
@@ -181,31 +186,139 @@ class Character:
             for move in selected:
                 self.moves.append(move)
 
+class App:
+
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.nameEntry = Entry(frame)
+        self.nameEntry.insert(0, "Insert Name Here")
+        self.nameEntry.grid(row=0, column=0)
+
+        self.tierScale = Scale(frame, from_=1, to=6, label='Pick Tier')
+        self.tierScale.grid(row=0, column=1)
+
+        self.classListbox = Listbox(frame, exportselection=0)
+        for cl in self.get_classes():
+            self.classListbox.insert(END, cl)
+        self.classListbox.select_set(0)
+        self.classListbox.bind('<<ListboxSelect>>', self.change_class)
+        self.classListbox.grid(row=1,column=0)
+
+        self.classText = StringVar()
+        self.classLabel = Label(frame, textvariable=self.classText, wraplength=100)
+        self.classLabel.grid(row=1,column=1)
+
+        self.fociListbox = Listbox(frame, exportselection=0)
+        for focus in self.get_foci():
+            self.fociListbox.insert(END, focus)
+        self.fociListbox.select_set(0)
+        self.fociListbox.bind('<<ListboxSelect>>', self.change_focus)
+        self.fociListbox.grid(row=2,column=0)
+
+        self.fociText = StringVar()
+        self.fociLabel = Label(frame, textvariable=self.fociText, wraplength=100)
+        self.fociLabel.grid(row=2,column=1)
+
+        self.descriptorListbox = Listbox(frame, exportselection=0)
+        for descriptor in self.get_descriptors():
+            self.descriptorListbox.insert(END, descriptor)
+        self.descriptorListbox.select_set(0)
+        self.descriptorListbox.bind('<<ListboxSelect>>', self.change_descriptor)
+        self.descriptorListbox.grid(row=3,column=0)
+
+        self.descriptorText = StringVar()
+        self.descriptorLabel = Label(frame, textvariable=self.descriptorText, wraplength=100)
+        self.descriptorLabel.grid(row=3,column=1)
+
+        self.doneButton = Button(frame, text="Done", command=self.basic_setup, width=100)
+        self.doneButton.grid(row=4,column=0,columnspan=5)
+
+        self.moveListbox = Listbox(frame, exportselection=0)
+        for move in self.get_moves():
+            self.moveListbox.insert(END, move)
+        self.moveListbox.select_set(0)
+        self.moveListbox.bind('<<ListboxSelect>>', self.change_move)
+        self.moveListbox.grid(row=1, column=2)
+
+        self.moveText = StringVar()
+        self.moveText = Label(frame, textvariable=self.moveText, wraplength=100)
+        self.moveLabel.grid(row=1, column=3)
+
+    def get_foci(self):
+        focus_conf = configparser.ConfigParser()
+        focus_conf.read('foci.ini', encoding='utf-8')
+        foci = focus_conf.sections()
+        return foci
+
+    def change_focus(self, picked):
+        listbox = picked.widget
+        focus_conf = configparser.ConfigParser()
+        focus_conf.read('foci.ini', encoding='utf-8')
+        focus = focus_conf[listbox.get(ACTIVE)]['description']
+        self.fociText.set(focus)
+
+    def get_descriptors(self):
+        descriptor_conf = configparser.ConfigParser()
+        descriptor_conf.read('descriptors.ini', encoding='utf-8')
+        descriptors = descriptor_conf.sections()
+        return descriptors
+
+    def change_descriptor(self, picked):
+        listbox = picked.widget
+        descriptor_conf = configparser.ConfigParser()
+        descriptor_conf.read('descriptors.ini', encoding='utf-8')
+        descriptor = descriptor_conf[listbox.get(ACTIVE)]['description']
+        self.descriptorText.set(descriptor)
+
+    def get_classes(self):
+        default_conf = configparser.ConfigParser()
+        default_conf.read('default.ini')
+        classes = list(dict(default_conf['Descriptions']).keys())
+        return classes
+    
+    def change_class(self, picked):
+        listbox = picked.widget
+        default_conf = configparser.ConfigParser()
+        default_conf.read('default.ini')
+        clas = default_conf['Descriptions'][listbox.get(ACTIVE)]
+        self.classText.set(clas)
+
+    def get_moves(self):
+        #TODO
+        return []
+    def change_move(self, picked):
+        #TODO
+        listbox = picked.widget
+        
+    def basic_setup(self):
+        name = self.nameEntry.get()
+        clas = self.classListbox.get(ACTIVE)
+        descriptor = self.descriptorListbox.get(ACTIVE)
+        focus = self.fociListbox.get(ACTIVE)
+        tier = self.tierScale.get()
+        for x in [name, clas, descriptor, focus, tier]:
+            if not x:
+                print("You haven't selected everything you need to")
+        char = Character(name, clas, tier)
+        
+        focus_conf = configparser.ConfigParser()
+        focus_conf.read('foci.ini', encoding='utf-8')
+        char.set_focus(focus, focus_conf)
+
+        descriptor_conf = configparser.ConfigParser()
+        descriptor_conf.read('descriptors.ini', encoding='utf-8')
+        char.set_descriptor(descriptor, descriptor_conf)
+        print(char)
+        
 # Simple commandline version
 if  __name__ == '__main__':
-    default_conf = configparser.ConfigParser()
-    default_conf.read('default.ini')
-    classes = list(dict(default_conf['Descriptions']).keys())
-    move_allowed = [int(default_conf['New_moves']['tier'+str(x)]) for x in range(1, 7)]
+    root = Tk()
 
-    # Basic information         
-    name = get_input("What's your name?")
-    clas = get_input('What class would you like to play?', restrictions = classes)
-    tier = get_input('What tier are you starting at?', restrictions = [str(x) for x in range(1, 7)])
-    char = Character(name, clas, tier)
-
-    # Focus
-    focus_conf = configparser.ConfigParser()
-    focus_conf.read('foci.ini', encoding='utf-8')
-    focus = get_input("What focus do you want?", restrictions = focus_conf.sections())
-    char.set_focus(focus, focus_conf)
-
-    # Descriptor
-    descriptor_conf = configparser.ConfigParser()
-    descriptor_conf.read('descriptors.ini', encoding='utf-8')
-    descriptor = get_input("What descriptor do you want?", restrictions = descriptor_conf.sections())
-    char.set_descriptor(descriptor, descriptor_conf)
-
+    app = App(root)
+    root.mainloop()
+else:
     # Picking moves
     for tier in range(1, char.tier+1):
         char.pick_moves(tier)
